b4408a914fae7f68c3c960c0e33f9edc9bed5562
二龙 <@ZHAOZHIHONG>
2020-12-10 15:42:46 +0800

    张航琦模型

---
+++ klopm/WebApplication1_APIYL/Medical.System.Base/ZZH_Server/PatientServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medical.System.Server.ZZH_Server
+{
+    public class PatientServer
+    {
+        
+    }
+}
---
+++ klopm/WebApplication1_APIYL/Medical.System.Model/ZHQ_Model/Charge1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medical.System.Model.ZHQ_Model
+{
+    /// <summary>
+    /// 收费表
+    /// </summary>
+    public class Charge1
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int ChargeId	 { get; set; }
+        /// <summary>
+        /// 收费商品名称
+        /// </summary>
+        public string ChargeName	 { get; set; }
+        /// <summary>
+        /// 商品单价
+        /// </summary>
+        public float ChargePrice { get; set; }
+        /// <summary>
+        /// 收费数量
+        /// </summary>
+        public int ChargeNum	 { get; set; }
+        /// <summary>
+        /// 应收金额
+        /// </summary>
+        public float ChargeYSJE { get; set; }
+    }
+}
